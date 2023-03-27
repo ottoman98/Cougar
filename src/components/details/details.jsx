32ec787d11274c2b607e93ea03d21e1102d9fe2d
= <=>
2023-03-27 01:04:16 -0500

    ajua

--- src/components/details/details.jsx
+++ src/components/details/details.jsx
@@ -3,6 +3,7 @@ import { useParams } from "react-router-dom";
 import useFetch from "../../hooks/useFetch";
 import Carosuel from "../carousel/corousel";
 import Gallery from "../gallery/gallery";
+import numberSeparator from "../../utils/thousands_separator";
 import './details.css';
 
 function Details() {
@@ -24,45 +25,78 @@ function Details() {
     return (<>
         {fetch ?
             <>
-                <div className="main-details">
+                <div id="main-product-page">
 
-                    <Gallery imgs={data.imgUrls} className="main-details-item" />
-                    <div className="main-details-item details-text">
-                        <h2>{data.nombre.charAt(0).toUpperCase() + data.nombre.slice(1)}</h2>
-                        <i>{data.precio} $</i><br />
+                    <div className="main-details">
 
-                        <i>Tallas</i>
-                        <div id="tallas">
+                        <Gallery imgs={data.imgUrls} className="main-details-item" />
+                        <div className="main-details-item ">
 
-                            {data.tallas.map((x) => {
-                                return (
-                                    <React.Fragment key={x}>
-                                        <div>{x}</div>
+                            <div id="product-info">
+                                <h2 className="name-product">{data.nombre.charAt(0).toUpperCase() + data.nombre.slice(1)}</h2>
 
-                                    </React.Fragment>);
-                            })}
+
+                                {data.descuento == 0 ?
+                                    <p className="antes-despues">
+                                        <span className="ahora-card">{numberSeparator(data.precio)} <span>$</span></span>
+                                    </p>
+                                    :
+                                    <p className="antes-ahora">
+
+                                        <span className="antes-card">{numberSeparator(data.precio)} <span>$</span></span>
+                                        <span className="ahora-card">{numberSeparator(data.precio * ((100 - data.descuento) / 100))} <span>$</span></span>
+
+                                    </p>
+                                }
+
+
+
+
+
+                                <h5 className="topic">Tallas</h5>
+                                <div className="topic-container">
+
+                                    {data.tallas.map((x) => {
+                                        return (
+                                            <React.Fragment key={x}>
+                                                <div>{x.toUpperCase()}</div>
+
+                                            </React.Fragment>);
+                                    })}
+                                </div>
+
+                                <h5 className="topic">Colores</h5>
+                                <div className="topic-container">
+
+                                    {data.colores.map((x) => {
+                                        return (
+                                            <React.Fragment key={x}>
+                                                <div>{x}</div>
+
+                                            </React.Fragment>);
+                                    })}
+                                </div>
+
+                            </div>
+                            <div className="description-details">
+                                <h3>Descripcion</h3>
+                                <p>
+                                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus rerum, sed possimus cumque, minus ratione exercitationem amet magni, dolorem quaerat in ipsum explicabo doloribus eligendi qui facilis molestias alias magnam?
+                                </p>
+                            </div>
+
+
                         </div>
 
-                        <p>Colores</p>
-                        <div id="colores">
-
-                            {data.colores.map((x) => {
-                                return (
-                                    <React.Fragment key={x}>
-                                        <div>{x}</div>
-
-                                    </React.Fragment>);
-                            })}
-                        </div>
 
                     </div>
-
-
+                    <Carosuel titulo="Completa  tu look" />
                 </div>
+
             </>
             : null}
 
-        <Carosuel titulo="Completa  tu look" />
+
 
 
     </>);
