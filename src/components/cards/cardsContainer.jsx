adff62e5337bd59237d08b6ebc1e3ae106556e90
= <=>
2023-04-11 00:10:42 -0500

    siuu

--- src/components/cards/cardsContainer.jsx
+++ src/components/cards/cardsContainer.jsx
@@ -10,7 +10,6 @@ function Cards (props) {
         <TopTemplate title='Nuestros destacados' content='mas destacados' />
 
         <div className='cards-container'>
-
           <div className='product-item'>
             <img className='product-img' src='https://assets.adidas.com/images/h_2000,f_auto,q_auto,fl_lossy,c_fill,g_auto/3e548ca32c7447daaf77afcd014ba31b_9366/Camiseta_Visitante_Seleccion_Colombia_Femenina_23_Multicolor_HS7544_01_laydown.jpg' alt='' />
 
@@ -18,6 +17,22 @@ function Cards (props) {
               <p id='product-brand'>adedos</p>
               <p>Camisea colombia</p>
               <p>100.000$</p>
+
+            </div>
+            <div className='data-hide'>
+              <div className='colores'>
+                <div className='color'> </div>
+                <div className='color'> </div>
+                <div className='color'> </div>
+                <div className='color'> </div>
+              </div>
+              <div className='tallas'>
+
+                <div className='talla'> S</div>
+                <div className='talla'> M</div>
+                <div className='talla'> L</div>
+
+              </div>
             </div>
 
           </div>
@@ -29,6 +44,22 @@ function Cards (props) {
               <p id='product-brand'>adedos</p>
               <p>Camisea colombia</p>
               <p>100.000$</p>
+
+            </div>
+            <div className='data-hide'>
+              <div className='colores'>
+                <div className='color'> </div>
+                <div className='color'> </div>
+                <div className='color'> </div>
+                <div className='color'> </div>
+              </div>
+              <div className='tallas'>
+
+                <div className='talla'> S</div>
+                <div className='talla'> M</div>
+                <div className='talla'> L</div>
+
+              </div>
             </div>
 
           </div>
@@ -40,6 +71,22 @@ function Cards (props) {
               <p id='product-brand'>adedos</p>
               <p>Camisea colombia</p>
               <p>100.000$</p>
+
+            </div>
+            <div className='data-hide'>
+              <div className='colores'>
+                <div className='color'> </div>
+                <div className='color'> </div>
+                <div className='color'> </div>
+                <div className='color'> </div>
+              </div>
+              <div className='tallas'>
+
+                <div className='talla'> S</div>
+                <div className='talla'> M</div>
+                <div className='talla'> L</div>
+
+              </div>
             </div>
 
           </div>
@@ -51,6 +98,22 @@ function Cards (props) {
               <p id='product-brand'>adedos</p>
               <p>Camisea colombia</p>
               <p>100.000$</p>
+
+            </div>
+            <div className='data-hide'>
+              <div className='colores'>
+                <div className='color'> </div>
+                <div className='color'> </div>
+                <div className='color'> </div>
+                <div className='color'> </div>
+              </div>
+              <div className='tallas'>
+
+                <div className='talla'> S</div>
+                <div className='talla'> M</div>
+                <div className='talla'> L</div>
+
+              </div>
             </div>
 
           </div>
@@ -62,6 +125,22 @@ function Cards (props) {
               <p id='product-brand'>adedos</p>
               <p>Camisea colombia</p>
               <p>100.000$</p>
+
+            </div>
+            <div className='data-hide'>
+              <div className='colores'>
+                <div className='color'> </div>
+                <div className='color'> </div>
+                <div className='color'> </div>
+                <div className='color'> </div>
+              </div>
+              <div className='tallas'>
+
+                <div className='talla'> S</div>
+                <div className='talla'> M</div>
+                <div className='talla'> L</div>
+
+              </div>
             </div>
 
           </div>
@@ -73,6 +152,292 @@ function Cards (props) {
               <p id='product-brand'>adedos</p>
               <p>Camisea colombia</p>
               <p>100.000$</p>
+
+            </div>
+            <div className='data-hide'>
+              <div className='colores'>
+                <div className='color'> </div>
+                <div className='color'> </div>
+                <div className='color'> </div>
+                <div className='color'> </div>
+              </div>
+              <div className='tallas'>
+
+                <div className='talla'> S</div>
+                <div className='talla'> M</div>
+                <div className='talla'> L</div>
+
+              </div>
+            </div>
+
+          </div>
+
+          <div className='product-item'>
+            <img className='product-img' src='https://assets.adidas.com/images/h_2000,f_auto,q_auto,fl_lossy,c_fill,g_auto/3e548ca32c7447daaf77afcd014ba31b_9366/Camiseta_Visitante_Seleccion_Colombia_Femenina_23_Multicolor_HS7544_01_laydown.jpg' alt='' />
+
+            <div className='product-data'>
+              <p id='product-brand'>adedos</p>
+              <p>Camisea colombia</p>
+              <p>100.000$</p>
+
+            </div>
+            <div className='data-hide'>
+              <div className='colores'>
+                <div className='color'> </div>
+                <div className='color'> </div>
+                <div className='color'> </div>
+                <div className='color'> </div>
+              </div>
+              <div className='tallas'>
+
+                <div className='talla'> S</div>
+                <div className='talla'> M</div>
+                <div className='talla'> L</div>
+
+              </div>
+            </div>
+
+          </div>
+
+          <div className='product-item'>
+            <img className='product-img' src='https://assets.adidas.com/images/h_2000,f_auto,q_auto,fl_lossy,c_fill,g_auto/3e548ca32c7447daaf77afcd014ba31b_9366/Camiseta_Visitante_Seleccion_Colombia_Femenina_23_Multicolor_HS7544_01_laydown.jpg' alt='' />
+
+            <div className='product-data'>
+              <p id='product-brand'>adedos</p>
+              <p>Camisea colombia</p>
+              <p>100.000$</p>
+
+            </div>
+            <div className='data-hide'>
+              <div className='colores'>
+                <div className='color'> </div>
+                <div className='color'> </div>
+                <div className='color'> </div>
+                <div className='color'> </div>
+              </div>
+              <div className='tallas'>
+
+                <div className='talla'> S</div>
+                <div className='talla'> M</div>
+                <div className='talla'> L</div>
+
+              </div>
+            </div>
+
+          </div>
+
+          <div className='product-item'>
+            <img className='product-img' src='https://assets.adidas.com/images/h_2000,f_auto,q_auto,fl_lossy,c_fill,g_auto/3e548ca32c7447daaf77afcd014ba31b_9366/Camiseta_Visitante_Seleccion_Colombia_Femenina_23_Multicolor_HS7544_01_laydown.jpg' alt='' />
+
+            <div className='product-data'>
+              <p id='product-brand'>adedos</p>
+              <p>Camisea colombia</p>
+              <p>100.000$</p>
+
+            </div>
+            <div className='data-hide'>
+              <div className='colores'>
+                <div className='color'> </div>
+                <div className='color'> </div>
+                <div className='color'> </div>
+                <div className='color'> </div>
+              </div>
+              <div className='tallas'>
+
+                <div className='talla'> S</div>
+                <div className='talla'> M</div>
+                <div className='talla'> L</div>
+
+              </div>
+            </div>
+
+          </div>
+
+          <div className='product-item'>
+            <img className='product-img' src='https://assets.adidas.com/images/h_2000,f_auto,q_auto,fl_lossy,c_fill,g_auto/3e548ca32c7447daaf77afcd014ba31b_9366/Camiseta_Visitante_Seleccion_Colombia_Femenina_23_Multicolor_HS7544_01_laydown.jpg' alt='' />
+
+            <div className='product-data'>
+              <p id='product-brand'>adedos</p>
+              <p>Camisea colombia</p>
+              <p>100.000$</p>
+
+            </div>
+            <div className='data-hide'>
+              <div className='colores'>
+                <div className='color'> </div>
+                <div className='color'> </div>
+                <div className='color'> </div>
+                <div className='color'> </div>
+              </div>
+              <div className='tallas'>
+
+                <div className='talla'> S</div>
+                <div className='talla'> M</div>
+                <div className='talla'> L</div>
+
+              </div>
+            </div>
+
+          </div>
+
+          <div className='product-item'>
+            <img className='product-img' src='https://assets.adidas.com/images/h_2000,f_auto,q_auto,fl_lossy,c_fill,g_auto/3e548ca32c7447daaf77afcd014ba31b_9366/Camiseta_Visitante_Seleccion_Colombia_Femenina_23_Multicolor_HS7544_01_laydown.jpg' alt='' />
+
+            <div className='product-data'>
+              <p id='product-brand'>adedos</p>
+              <p>Camisea colombia</p>
+              <p>100.000$</p>
+
+            </div>
+            <div className='data-hide'>
+              <div className='colores'>
+                <div className='color'> </div>
+                <div className='color'> </div>
+                <div className='color'> </div>
+                <div className='color'> </div>
+              </div>
+              <div className='tallas'>
+
+                <div className='talla'> S</div>
+                <div className='talla'> M</div>
+                <div className='talla'> L</div>
+
+              </div>
+            </div>
+
+          </div>
+
+          <div className='product-item'>
+            <img className='product-img' src='https://assets.adidas.com/images/h_2000,f_auto,q_auto,fl_lossy,c_fill,g_auto/3e548ca32c7447daaf77afcd014ba31b_9366/Camiseta_Visitante_Seleccion_Colombia_Femenina_23_Multicolor_HS7544_01_laydown.jpg' alt='' />
+
+            <div className='product-data'>
+              <p id='product-brand'>adedos</p>
+              <p>Camisea colombia</p>
+              <p>100.000$</p>
+
+            </div>
+            <div className='data-hide'>
+              <div className='colores'>
+                <div className='color'> </div>
+                <div className='color'> </div>
+                <div className='color'> </div>
+                <div className='color'> </div>
+              </div>
+              <div className='tallas'>
+
+                <div className='talla'> S</div>
+                <div className='talla'> M</div>
+                <div className='talla'> L</div>
+
+              </div>
+            </div>
+
+          </div>
+
+          <div className='product-item'>
+            <img className='product-img' src='https://assets.adidas.com/images/h_2000,f_auto,q_auto,fl_lossy,c_fill,g_auto/3e548ca32c7447daaf77afcd014ba31b_9366/Camiseta_Visitante_Seleccion_Colombia_Femenina_23_Multicolor_HS7544_01_laydown.jpg' alt='' />
+
+            <div className='product-data'>
+              <p id='product-brand'>adedos</p>
+              <p>Camisea colombia</p>
+              <p>100.000$</p>
+
+            </div>
+            <div className='data-hide'>
+              <div className='colores'>
+                <div className='color'> </div>
+                <div className='color'> </div>
+                <div className='color'> </div>
+                <div className='color'> </div>
+              </div>
+              <div className='tallas'>
+
+                <div className='talla'> S</div>
+                <div className='talla'> M</div>
+                <div className='talla'> L</div>
+
+              </div>
+            </div>
+
+          </div>
+
+          <div className='product-item'>
+            <img className='product-img' src='https://assets.adidas.com/images/h_2000,f_auto,q_auto,fl_lossy,c_fill,g_auto/3e548ca32c7447daaf77afcd014ba31b_9366/Camiseta_Visitante_Seleccion_Colombia_Femenina_23_Multicolor_HS7544_01_laydown.jpg' alt='' />
+
+            <div className='product-data'>
+              <p id='product-brand'>adedos</p>
+              <p>Camisea colombia</p>
+              <p>100.000$</p>
+
+            </div>
+            <div className='data-hide'>
+              <div className='colores'>
+                <div className='color'> </div>
+                <div className='color'> </div>
+                <div className='color'> </div>
+                <div className='color'> </div>
+              </div>
+              <div className='tallas'>
+
+                <div className='talla'> S</div>
+                <div className='talla'> M</div>
+                <div className='talla'> L</div>
+
+              </div>
+            </div>
+
+          </div>
+
+          <div className='product-item'>
+            <img className='product-img' src='https://assets.adidas.com/images/h_2000,f_auto,q_auto,fl_lossy,c_fill,g_auto/3e548ca32c7447daaf77afcd014ba31b_9366/Camiseta_Visitante_Seleccion_Colombia_Femenina_23_Multicolor_HS7544_01_laydown.jpg' alt='' />
+
+            <div className='product-data'>
+              <p id='product-brand'>adedos</p>
+              <p>Camisea colombia</p>
+              <p>100.000$</p>
+
+            </div>
+            <div className='data-hide'>
+              <div className='colores'>
+                <div className='color'> </div>
+                <div className='color'> </div>
+                <div className='color'> </div>
+                <div className='color'> </div>
+              </div>
+              <div className='tallas'>
+
+                <div className='talla'> S</div>
+                <div className='talla'> M</div>
+                <div className='talla'> L</div>
+
+              </div>
+            </div>
+
+          </div>
+
+          <div className='product-item'>
+            <img className='product-img' src='https://assets.adidas.com/images/h_2000,f_auto,q_auto,fl_lossy,c_fill,g_auto/3e548ca32c7447daaf77afcd014ba31b_9366/Camiseta_Visitante_Seleccion_Colombia_Femenina_23_Multicolor_HS7544_01_laydown.jpg' alt='' />
+
+            <div className='product-data'>
+              <p id='product-brand'>adedos</p>
+              <p>Camisea colombia</p>
+              <p>100.000$</p>
+
+            </div>
+            <div className='data-hide'>
+              <div className='colores'>
+                <div className='color'> </div>
+                <div className='color'> </div>
+                <div className='color'> </div>
+                <div className='color'> </div>
+              </div>
+              <div className='tallas'>
+
+                <div className='talla'> S</div>
+                <div className='talla'> M</div>
+                <div className='talla'> L</div>
+
+              </div>
             </div>
 
           </div>
--- src/components/highlighted/highlighted.jsx
+++ src/components/highlighted/highlighted.jsx
@@ -11,14 +11,17 @@ function HighLighted () {
 
           <div className='img-div first-img-1'>
 
-            <img className='img-high' src={img} alt='' />
+            <img className='img-high' src='https://cdn.webshopapp.com/shops/277570/files/365354292/image.jpg' alt='' />
             <p>Camisetas</p>
 
           </div>
 
           <div className='img-div first-img-2'>
 
-            <img className='img-high' src={img} alt='' />
+            <img
+              className='img-high' src='
+              https://cdn.webshopapp.com/shops/277570/files/347910875/image.jpg' alt=''
+            />
             <p>Camisetas</p>
 
           </div>
@@ -37,6 +40,14 @@ function HighLighted () {
             <p>Camisetas</p>
 
           </div>
+
+          <div className='img-div'>
+
+            <img className='img-high first-img' src={img} alt='' />
+            <p>Camisetas</p>
+
+          </div>
+
           <div className='img-div'>
 
             <img className='img-high first-img' src={img} alt='' />
