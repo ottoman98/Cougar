cdae0a5e24e7c319cb3fa3d76196c7fbc5967617
= <=>
2023-04-12 07:10:31 -0500

    siu

--- src/components/preabout/preaAbout.jsx
+++ src/components/preabout/preaAbout.jsx
@@ -9,8 +9,8 @@ function PreAbout () {
         <div className='about-us'>
           <h3>Trabajando por ti</h3>
           <p>
-            <p className='run'>   ¡Bienvenidos a nuestra tienda de ropa deportiva! Somos una empresa con 5 años de experiencia en la venta de ropa deportiva de alta calidad y diseño innovador. Nos enorgullece ofrecer una amplia variedad de productos, incluyendo ropa deportiva para todas las edades y estilos de vida.</p>
-            animi, tempore unde aliquam facere in sit quam aspernatur odit, eius exercitationem laudantium ratione. Minus esewefasdwe asdweasdfweasd wasd t eos enim. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Veniam aut obcaecati quasi. Minima molestiae ratione ut sequi ad tenetur dolore quasi adipisci rem, error earum amet corrupti consequatur omnis qui.
+            <p className='bold-text'>   ¡Bienvenidos a nuestra tienda de ropa deportiva! Somos una incluyendo ropa deportiva para todas las edades y estilos de vida.</p>
+            animi, tempore unde aliquam facere in sit quam aspernatur odit, eius exer
             <br />
             <p className='button-more'>Lee nuesta historia
             </p>
